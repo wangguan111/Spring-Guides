d28d581f0ff870fc2dc04f1132d596429e033984
wangguan111 <wang_guan111@126.com>
2018-07-19 00:19:22 +0800

    5.Uploading Files

---
+++ uploading/src/main/java/com/example/uploading/storage/StorageProperties.java
@@ -0,0 +1,24 @@
+package com.example.uploading.storage;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("storage")
+public class StorageProperties {
+
+    /**
+     * Folder location for storing files
+     */
+    private String location = "upload-dir";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
